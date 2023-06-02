06b14f6c765081976fa5a3687247befc3b3c0418
Pablo Sosa <pvsrod@gmail.com>
2023-06-02 17:07:58 -0500

    change back to original API url's

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -39,8 +39,8 @@ export const services = [
 	},
 ];
 
-//export const products_url = "https://course-api.com/react-store-products";
-export const products_url = "http://localhost:3004/products";
+export const products_url = "https://course-api.com/react-store-products";
+//export const products_url = "http://localhost:3004/products";
 
-//export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
-export const single_product_url = `http://localhost:3005/products/`;
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
+//export const single_product_url = `http://localhost:3005/products/`;
